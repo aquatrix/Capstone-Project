e1cbad0ed4447975f7ef364f1c57a7cef2138143
Mihir Jain <mihirshah0201@gmail.com>
2024-06-22 23:47:12 -0400

    Updating the Sprint 1

--- Car Swap/Scripts/profile.js
+++ Car Swap/Scripts/profile.js
@@ -1,124 +1,248 @@
-let profileData = document.getElementById("profile-data");
-let profileCard = document.getElementById("profile-card");
-let saveButton = document.getElementById("save-button");
+import {
+  auth,
+  onAuthStateChanged,
+  get,
+  child,
+  dbRef,
+  database,
+  signOut,
+  uploadBytes,
+  storageRef,
+  getDownloadURL,
+  storage,
+  set,
+  update,
+} from "./firebaseConfig.js";
 
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
+let profileName = document.getElementById("profile-name");
+let profileUserName = document.getElementById("profile-username");
+let profileImg = document.getElementById("profileImg");
+let newImage = document.getElementById("new-profile");
 
-let emailInput = document.getElementById("emailInput");
+let emailAddress = document.getElementById("email");
+let city = document.getElementById("city");
+let state = document.getElementById("state");
+let province = document.getElementById("province");
 
-let cityName = document.getElementById("city-name");
+let firstName = document.getElementById("input-f-name");
+let lastName = document.getElementById("input-l-name");
 
-let provinceDropdown = document.getElementById("province-dropdown");
+let cityName = document.getElementById("input-city");
 
-const logoutBtn = document.getElementById("logout-button");
+let editProfileButton = document.getElementById("edit-btn");
+let profileSection = document.getElementById("profile-section");
+let editButton = document.getElementById("edit-button");
 
+let editProfile = document.getElementById("edit-profile");
+let saveBtn = document.getElementById("save-btn");
+let cancelBtn = document.getElementById("cancel");
 
-firebase.auth().onAuthStateChanged(function (user) {
+let uploadBtn = document.getElementById("uploadPicture");
+
+let cardContainer = document.getElementById("card-container");
+
+let uid = "";
+
+onAuthStateChanged(auth, function (user) {
   if (user) {
-    let uid = user.uid;
-    firebase
-      .database()
-      .ref(`users/${uid}`)
-      .on(
-        "value",
-        (snapshot) => {
-          const userName = snapshot.val();
-          console.log(userName);
+    console.log("User is signed in:", user.uid);
+    uid = user.uid;
+    const ref = dbRef(database);
+    get(child(ref, `users/${user.uid}`))
+      .then((snapshot) => {
+        const user = snapshot.val();
+        console.log(user.firstName);
+        profileName.textContent = `${user.firstName}'s Profile`;
+        profileUserName.innerHTML = `${user.firstName} ${user.lastName}`;
+        emailAddress.innerHTML = user.email;
+        city.innerHTML = user.cityName;
+        state.innerHTML = user.province;
 
-          profileData.innerHTML = `<p>First Name: ${userName.firstName}</p>
-              <p>Last Name: ${userName.lastName}</p>
-              <p>Email: ${userName.email}</p>
-              <p>City: ${userName.cityName}</p>
-              <p>Province: ${userName.province}</p>
-              <button type="button" id="edit-button" class="btn btn-warning">Edit Profile</button>
-`;
-          firstName.value = userName.firstName;
-          lastName.value = userName.lastName;
-          emailInput.value = userName.email;
-          cityName.value = userName.cityName;
-          provinceDropdown.value = userName.province;
-          let editButton = document.getElementById("edit-button");
-          editButton.addEventListener("click", function () {
-            form.style.display = "block";
-            avatar.style.display = "none";
-            profileCard.style.border = "none";
-            profileData.style.display = "none";
-          });
-          console.log(editButton);
-        },
-        (error) => {
-          console.error("Error reading data:", error);
-        }
-      );
-  } else {
-    console.log("No user signed in");
-  }
-});
-
-let form = document.getElementById("signup-form");
-let avatar = document.getElementById("avatar");
-avatar.style.display = "flex";
-form.style.display = "none";
-profileCard.style.border = "block";
-profileData.style.display = "block";
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  
-});
-
-saveButton.addEventListener("click", saveUserData);
-
-function saveUserData() {
-  let user = firebase.auth().currentUser;
-  if (user) {
-    let uid = user.uid;
-    let userData = {
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: emailInput.value,
-      cityName: cityName.value,
-      province: provinceDropdown.value,
-    };
-
-    firebase
-      .database()
-      .ref(`users/${uid}`)
-      .set(userData)
-      .then(() => {
-        console.log("User data updated successfully");
-        form.style.display = "none";
-        profileCard.style.border = "1px solid rgb(200, 200, 200";
-        profileData.style.display = "block";
-        avatar.style.display = "block";
-
-        
+        profileImg.src = user.profileImage;
       })
       .catch((error) => {
-        console.error("Error updating user data:", error);
+        console.error("Error fetching user data:", error);
       });
   } else {
-    console.log("No user is signed in");
+    console.log("No user is signed in.");
   }
-}
-
-
-
-
-logoutBtn.addEventListener("click", function (e) {
-  e.preventDefault(); 
-
-  firebase.auth().signOut().then(() => {
-    console.log("User signed out.");
-    window.location.href = "./login.html"; 
-  }).catch((error) => {
-    console.error("Sign out error:", error);
-  });
 });
 
-window.addEventListener('popstate', function (event) {
-  if (!firebase.auth().currentUser) {
-    window.location.href = "./login.html";
+let logoutButton = document.getElementById("logout-button");
+
+logoutButton.addEventListener("click", function () {
+  logoutButton.disabled = true;
+  logoutButton.textContent = "Logging Out...";
+
+  setTimeout(function () {
+    signOut(auth)
+      .then(function () {
+        console.log("User signed out.");
+
+        setTimeout(function () {
+          window.location.href = "./login.html";
+        }, 800);
+      })
+      .catch(function (error) {
+        console.error("Sign out error:", error);
+      });
+  }, 800);
+});
+
+editProfileButton.addEventListener("click", function () {
+  profileSection.style.display = "none";
+  editButton.style.display = "none";
+  editProfile.style.display = "flex";
+  saveBtn.style.display = "flex";
+
+  const ref = dbRef(database);
+  get(child(ref, `users/${uid}`))
+    .then((snapshot) => {
+      const user = snapshot.val();
+      newImage.src = user.profileImage;
+      firstName.value = user.firstName;
+      lastName.value = user.lastName;
+      cityName.value = user.cityName;
+      province.value = user.province;
+      console.log(user.cityName);
+      console.log(user.province);
+      //province.value =
+    })
+    .catch((error) => {
+      console.error("Error fetching user data:", error);
+    });
+});
+
+saveBtn.addEventListener("click", function () {
+  console.log(lastName.value);
+  console.log(province.value);
+  console.log(cityName.value);
+  console.log(firstName.value);
+  console.log("click");
+
+  // Update the user data in Firebase
+  const updatedUserData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    cityName: cityName.value,
+    province: province.value,
+  };
+
+  const userRef = dbRef(database, `users/${uid}`);
+
+  update(userRef, updatedUserData)
+    .then(() => {
+      console.log("User data updated successfully!");
+
+      profileSection.style.display = "flex";
+      editButton.style.display = "flex";
+      editProfile.style.display = "none";
+      saveBtn.style.display = "none";
+    })
+    .catch((error) => {
+      console.error("Error updating user data:", error);
+    });
+  location.reload();
+});
+
+cancelBtn.addEventListener("click", function () {
+  profileSection.style.display = "flex";
+  editButton.style.display = "flex";
+  editProfile.style.display = "none";
+  saveBtn.style.display = "none";
+});
+
+// Uploading Profile Picture in Database and in the User node is completed
+uploadBtn.addEventListener("click", () => {
+  console.log("Upload clicked...");
+  let picture = document.createElement("input");
+  picture.type = "file";
+
+  picture.addEventListener("change", (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      const storageReference = storageRef(
+        storage,
+        "profileImg/" + selectedFile.name
+      );
+
+      uploadBytes(storageReference, selectedFile)
+        .then((snapshot) => {
+          console.log("Image uploaded successfully!");
+          getDownloadURL(snapshot.ref)
+            .then((url) => {
+              console.log(url);
+              newImage.src = url;
+
+              var userRef = dbRef(database, `users/${uid}/profileImage`);
+
+              set(userRef, url)
+                .then(() => {
+                  console.log("Profile image URL updated in database");
+                })
+                .catch((error) => {
+                  console.error("Error updating profile image URL:", error);
+                });
+            })
+            .catch((error) => {
+              console.error("Error getting download URL", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error uploading image", error);
+        });
+    } else {
+      alert("Please select an image to upload.");
+    }
+  });
+  picture.click();
+});
+
+onAuthStateChanged(auth, function (user) {
+  if (user) {
+    const ref = dbRef(database);
+
+    get(child(ref, `users/${user.uid}/myListings`))
+      .then((snapshot) => {
+        const carListings = snapshot.val();
+        if (carListings) {
+          Object.keys(carListings).forEach((listingKey) => {
+            const listing = carListings[listingKey];
+            console.log("Listing Key:", listingKey);
+            console.log("Listing Details:", listing);
+            cardContainer.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div class="card" style="width: 100%">
+              <img src="${listing.image}" class="card-img-top" alt="Card image"/>
+              <div class="card-body">
+                <h5 class="card-title">${listing.make} ${listing.model}</h5>
+                <p class="card-text">
+                  ${listing.color} ${listing.year} ${listing.make} ${listing.model} with ${listing.milage} kilometres, priced at $${listing.price}.
+                </p>
+                <a href="cardetails.html" id=${listingKey} class="btn btn-primary view-details">View Details</a>
+              </div>
+            </div>
+          </div>`;
+          });
+
+          var buttons = document.querySelectorAll(".view-details");
+          buttons.forEach(function (button) {
+            button.addEventListener("click", function (event) {
+              event.preventDefault();
+              const carId = button.id;
+              console.log("Button clicked for car ID:", carId);
+              localStorage.setItem("selectedCarId", carId);
+              window.location.href = "./cardetails.html";
+            });
+          });
+        } else {
+          console.log("No listings found.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching car listings:", error);
+      });
+  } else {
+    console.log("No user is signed in.");
   }
 });
